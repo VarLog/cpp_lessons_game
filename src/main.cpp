77378d9e649b7561e5ab9067fef15dca2d6f2df1
Maxim Fedorenko <varlllog@gmail.com>
2016-12-03 15:38:49 +0300

    Add score

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,18 @@ class Game
 
         auto answer = 0;
 
+        auto score = 0;
+
         std::cout << a << " + " << b << std::endl;
 
         std::cin >> answer;
 
         if(answer == a + b)
         {
+            ++score;
+
             std::cout << "Correct!" << std::endl;
+            std::cout << "Your score: " << score << std::endl;
         }
         else
         {
