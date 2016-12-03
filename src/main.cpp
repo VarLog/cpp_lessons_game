c855890b7f2350f02a6eab8a275afe31eb108e30
Maxim Fedorenko <varlllog@gmail.com>
2016-12-03 15:31:58 +0300

    Implement Game::run()

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,24 @@ class Game
 {
     int run()
     {
-        /// \todo implement it
+        auto a = 2;
+        auto b = 4;
+
+        auto answer = 0;
+
+        std::cout << a << " + " << b << std::endl;
+
+        std::cin >> answer;
+
+        if(answer == a + b)
+        {
+            std::cout << "Correct!" << std::endl;
+        }
+        else
+        {
+            std::cout << "Mistake!" << std::endl;
+        }
+
         return 0;
     }
 };
